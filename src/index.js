f2b592b1536219a929e26eb5e0f9fd974cebb506
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-03-15 12:18:44 +0300

    res ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return (arguments.length === 0 || array.length === 0) ? 0 : Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return (arguments.length === 0 || array.length === 0) ? 0 : Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    // return (arguments.length === 0 || array.length === 0) ? 0 : array.reduce((a,b,array)=> ((a+b) / array.length));
+    let sum = 0;
+    let res;
+    if(arguments.length === 0 || array.length === 0){
+        return 0;
+    } else {
+        sum = array.reduce((a,b,array)=>(a+b));
+        res = sum / array.length;
+    }
+    return res;
 }
